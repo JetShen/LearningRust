a9229b1d45917b42dc61429a1a1ba47285889a0f
JetShen <fernandoarielthielelepe@gmail.com>
2023-05-20 12:26:14 -0400

    20/05/23

---
+++ Simple Exercises/cap 3/control_flow/src/main.rs
@@ -0,0 +1,64 @@
+fn main() {
+    // let number = 6;
+
+    // if number % 4 == 0 {
+    //     println!("number is divisible by 4");
+    // } else if number % 3 == 0 {
+    //     println!("number is divisible by 3");
+    // } else if number % 2 == 0 {
+    //     println!("number is divisible by 2");
+    // } else {
+    //     println!("number is not divisible by 4, 3, or 2");
+    // }
+
+    // let condition = false;
+    // let number = if condition { 5 } else { 6 };
+
+    // println!("The value of number is: {number}");
+
+
+
+    // let mut counter = 0;
+    
+    // let result = loop {
+    //     counter += 1;
+
+    //     if counter == 10 {
+    //         break counter * 2;
+    //     }
+    // };
+
+    // println!("The result is {result}");
+
+
+
+
+
+    let mut count = 0;
+    'counting_up: loop { //the { ' } indicates that this is a label and not a loop name
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a { //for element in a.iter()
+        println!("the value is: {element}");
+    }
+}
