ac56bd491b12bf7e7a03a44e6c6a5c34a33ba7c8
JetShen <fernandoarielthielelepe@gmail.com>
2023-05-21 17:16:24 -0400

    21/05/23

---
+++ Simple Exercises/cap 4/references_borrowing/src/main.rs
@@ -0,0 +1,36 @@
+// fn main() {
+//     let s1 = String::from("hello");
+
+//     let len = calculate_length(&s1);
+
+//     println!("The length of '{}' is {}.", s1, len);
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+
+
+// fn main() {
+//     let mut s = String::from("hello");
+
+//     change(&mut s);
+//     println!("{}", s);
+// }
+
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
+
+fn main(){
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+}
