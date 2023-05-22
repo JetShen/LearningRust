a52f92583ee8f7be3957588910c32ee0ad4e954a
JetShen <fernandoarielthielelepe@gmail.com>
2023-05-22 17:50:43 -0400

    22/05/23

---
+++ Simple Exercises/cap 5/example_struc/src/main.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)] // This annotation allows us to print the struct
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {:?}", rect1); // {:?} is used to print the struct
+}
